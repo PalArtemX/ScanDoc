db8dacfd98a82785d7d44b50ccd1af1e0189fc35
Artem Palyutin <76967266+PalArtemX@users.noreply.github.com>
2021-08-23 21:37:14 +0300

    Initial Commit

---
+++ ScannerDoc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScannerDoc
+//
+//  Created by Artem Palyutin on 23.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScannerDoc/ScannerDocApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerDocApp.swift
+//  ScannerDoc
+//
+//  Created by Artem Palyutin on 23.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct ScannerDocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
